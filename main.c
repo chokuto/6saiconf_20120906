43c3d28e3c3ba7ff6a0e082dae851d5d48c14645
chokuto <chokutto@gmail.com>
2012-09-06 22:44:36 +0900

    はい、最後にラスボス、「ディレイ」を実装しちゃいましょう。 これは非常に手ごわい相手ですが、音がかなり幻想的な雰囲気になるので、挑戦しちゃいましょう！
    鳴らしてみてください。お風呂場みたいな雰囲気？

--- main.c
+++ main.c
@@ -12,6 +12,8 @@ int main(void)
 	double x = 0.0;
 	double y = 0.0;
 	double z = 0.0;
+	unsigned char buff[44100 / 4] = {0};
+	int p = 0;
 	fwrite(header, 1, 44, fp);
 	for (i = 0; i < 16; i++)
 	{
@@ -19,7 +21,9 @@ int main(void)
 		scanf("%d", &key);
 		for (j = 0; j < 44100 / 4; j++)
 		{
-			unsigned char a = (unsigned char)((sin(x) >= 0.0 ? 1.0 : -1.0) * 64.0 + sin(z) * 63.0 + 128.0);
+			unsigned char a = ((unsigned char)((sin(x) >= 0.0 ? 1.0 : -1.0) * 64.0 + sin(z) * 63.0 + 128.0) + buff[p] * 3) / 4;
+			buff[p] = a;
+			p = (p + 1) % (44100 / 4);
 			fwrite(&a, 1, 1, fp);
 			x += (441.0 + sin(y) * 10.0) * pow(2.0, (double)key / 12.0) * 2.0 * 3.14159265358979 / 44100.0;
 			y += 5.0 * 2.0 * 3.14159265358979 / 44100.0;
